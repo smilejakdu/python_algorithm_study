4d7a766c7acd3c3fdde2ee3de076acdf25111365
smilejakdu <ash982416@gmail.com>
2020-07-28 10:07:00 +0900

    Twosum_smilejakdu

--- smilejakdu/TwoSum.py
+++ smilejakdu/TwoSum.py
@@ -1,10 +1,5 @@
 class Solution(object):
     def twoSum(self, nums, target):
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: List[int]
-        """
         for n1 in range(0 , len(nums)):
             for n2 in range(1 , len(nums)):
                 if nums[n1]+nums[n2] == target and n1 != n2:
